dcb56d6989ac41410a46de48244543488445d567
gabin <18649660797@163.com>
2020-10-10 20:09:57 +0800

    做授权公钥获取的延迟加载

--- gateway/src/main/java/top/gabin/demo/gateway/config/ResourceServerConfig.java
+++ gateway/src/main/java/top/gabin/demo/gateway/config/ResourceServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -20,6 +21,7 @@ import top.gabin.demo.gateway.component.RestAuthenticationEntryPoint;
 import top.gabin.demo.gateway.component.RestfulAccessDeniedHandler;
 import top.gabin.demo.gateway.constant.AuthConstant;
 
+import java.net.HttpURLConnection;
 import java.util.List;
 
 @AllArgsConstructor
@@ -59,7 +61,31 @@ public class ResourceServerConfig {
     @Bean
     public ReactiveJwtDecoder reactiveJwtDecoder() {
         // 比较尴尬的是，这里要先知道授权服务器地址
-        return new NimbusReactiveJwtDecoder("http://localhost:9400/rsa/publicKey");
+//        return new NimbusReactiveJwtDecoder("http://localhost:9400/rsa/publicKey");
+
+        // 做延迟加载，缺点是服务中心和网关都要先起来，这期间如果先调用就完蛋蛋了，不过怎么说呢，这个其实也有改进的空间，总之是个思路
+        // 注意，我这边用的地址是网关转发的地址，理论上是有负载均衡的东西在里面，和上面那个9400不一样，接口都有了，自己不懂得扩展，那没办法，
+        // 总不可能别人什么都帮你写好了
+        return new ReactiveJwtDecoder() {
+            private volatile NimbusReactiveJwtDecoder nimbusReactiveJwtDecoder;
+            @Override
+            public Mono<Jwt> decode(String token) throws JwtException {
+                NimbusReactiveJwtDecoder decoder = getNimbusReactiveJwtDecoder();
+                return decoder.decode(token);
+            }
+
+            private NimbusReactiveJwtDecoder getNimbusReactiveJwtDecoder() {
+                if (nimbusReactiveJwtDecoder == null) {
+                    synchronized (this) {
+                        if (nimbusReactiveJwtDecoder == null) {
+                            // 这里做延迟加载
+                            nimbusReactiveJwtDecoder = new NimbusReactiveJwtDecoder("http://localhost:9080/auth/rsa/publicKey");
+                        }
+                    }
+                }
+                return nimbusReactiveJwtDecoder;
+            }
+        };
     }
 
 }
